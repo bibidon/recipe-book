638e86f9163ac700bdda04a5b058e84ac684630b
bibidon <debibidon@gmail.com>
2016-09-19 22:33:47 +0300

    Finish Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,46 @@ import { FormsModule } from "@angular/forms"
 
 import { RecipeBookComponent } from "./recipe-book.component";
 import { HeaderComponent } from "./header.component";
+import { DropdownDirective } from "./dropdown.directive";
 import { ShoppingListAddComponent } from "./shopping-list/shopping-list-add.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item.component";
+import { RecipeStartComponent } from "./recipes/recipe-start.component";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import {APP_ROUTES_PROVIDERS} from "./app.routes";
+import { RecipeService } from "./recipes/recipe.service";
+//import {APP_ROUTES_PROVIDERS} from "./app.routes";
+import { routing } from "./app.routing";
 
 @NgModule({
   declarations: [
     RecipeBookComponent,
     HeaderComponent,
+    DropdownDirective,
     ShoppingListAddComponent,
     ShoppingListComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeEditComponent,
+    RecipeItemComponent,
+    RecipeStartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   bootstrap: [
     RecipeBookComponent
   ],
-  providers: [ShoppingListService, APP_ROUTES_PROVIDERS]
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    //APP_ROUTES_PROVIDERS
+  ]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
-import { RecipeBookComponent, environment } from './app/';
+import { environment } from './app/';
 import { AppModule } from './app/app.module';
-import { ShoppingListService } from "./app/shopping-list";
 
 if (environment.production) {
   enableProdMode();
